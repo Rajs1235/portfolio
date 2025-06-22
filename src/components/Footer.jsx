f73ac62fe1ef52e7176120b38805f09ae0016e7b
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-22 19:09:29 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,15 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/r_ajsrivastava?igsh=MWpmODF1bGVyNTg4"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
-        <FaXTwitter size={24} />
-      </a>
+
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/raj-srivastava-a680482b4"
         target="_blank"
         rel="noopener noreferrer"
       >
