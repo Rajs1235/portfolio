ac6b0297ad5480d9027c881d5da5f8423d3d95c2
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-22 18:59:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -72,8 +72,8 @@ const About = () => {
       </div>
 
       <p ref={paragraphRef} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in  JavaScript, and expertise in frameworks 
-        like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, 
+        I'm a skilled Engineering Student with experience in  C,C++,Python,JavaScript, and expertise in frameworks 
+        like React, Node.js. I'm a quick learner and collaborate closely with clients to create efficient, 
         scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
       </p>
 
